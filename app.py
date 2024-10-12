e20d7acca16b009d7b2cf7e1f4607c180b481803
Ирина Бушуева <bushuevvvva@yandex.ru>
2024-10-12 18:38:30 +0700

    Фраза

--- app.py
+++ app.py
@@ -208,6 +208,12 @@ def example():
             {'name':'манго' , 'price': 321}
     ]
     return render_template('example.html', name = name, num = num, curs = curs, gruppa = gruppa, fruits = fruits)
+
 @app.route('/lab2/')
 def lab2():
       return render_template('lab2.html')
+
+@app.route('/lab2/filters')
+def filters():
+      phrase = "О <b> скольно </b> <u>нам</u> <i>открытий</i> чудных..."
+      return render_template('filter.html', phrase = phrase)
