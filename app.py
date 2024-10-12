447f37ed69a4a9ac67506f31630103eef0ab4805
Ирина Бушуева <bushuevvvva@yandex.ru>
2024-10-12 17:31:00 +0700

    Вывод цветка

--- app.py
+++ app.py
@@ -164,3 +164,21 @@ def kapibara():
     </body>
 </html>
 '''
+
+# -----------------------------------------------------------
+
+@app.route('/lab2/a')
+def a():
+      return "без слэша"
+
+@app.route('/lab2/a/')
+def a2():
+      return "со слэшем"
+
+flower_list = ('роза','тюльпан', 'незабудка','ромашка')
+@app.route('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+      if flower_id >= len(flower_list):
+            return 'нет такого цветка', 404
+      else:
+            return "цветок:" + flower_list[flower_id]
