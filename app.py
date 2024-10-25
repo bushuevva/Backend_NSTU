5eb87f5890ad0c0635978de28585d4c031d88837
Ирина Бушуева <bushuevvvva@yandex.ru>
2024-10-25 15:46:33 +0700

    Заготовка к третьей лабораторной работе

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, redirect, url_for, render_template
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 
 @app.route('/')
 @app.route('/index')
@@ -29,8 +31,11 @@ def menu():
         <div>
             <a href='http://127.0.0.1:5000/lab1'>Лабораторная работа 1</a>
         </div>
-                <div>
+        <div>
             <a href='http://127.0.0.1:5000/lab2'>Лабораторная работа 2</a>
+        </div>
+                <div>
+            <a href='http://127.0.0.1:5000/lab3'>Лабораторная работа 3</a>
         </div>
 
         <footer>
