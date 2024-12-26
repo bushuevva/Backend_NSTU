5050902f87b78512f7b7ed5f101ce73863ac6de9
Ирина Бушуева <bushuevvvva@yandex.ru>
2024-12-27 03:33:21 +0700

    Логаут

--- lab8.py
+++ lab8.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, request
 from db import db
 from db.models import users,articles
 from werkzeug.security import check_password_hash, generate_password_hash
-from flask_login import login_user, login_required, current_user
+from flask_login import login_user, login_required, current_user, logout_user
 
 lab8 = Blueprint('lab8', __name__)
 @lab8.route('/lab8/')
@@ -55,7 +55,16 @@ def login():
         
     return render_template('/lab8/login.html',
                            error = 'Ошибка входа: логин и/или пароль неверны')
+
+
 @lab8.route('/lab8/articles/')
 @login_required
 def article_list():
     return 'Спиоск статей'
+
+
+@lab8.route('/lab8/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/lab8/')
